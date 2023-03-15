875ff2b5f8d66d418c1d38e32dbfbc6c9a78e846
Brasseur Thibault <t.brasseur@activigie.com>
2023-03-15 01:08:05 +0100

    fix(boostrap_css) : init css boostrap components

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="secondary" size="lg">
+          sociocratie.gouv
+        </Button>
       </header>
     </div>
   );
